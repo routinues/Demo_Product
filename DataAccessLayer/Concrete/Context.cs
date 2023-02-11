438024cd36132821dc702d30ddfd389967d1c09e
routinues <routinues@gmail.com>
2023-02-12 00:17:02 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SQL Server Bağlantı Kodu
+            optionsBuilder.UseSqlServer("server=ROUTINUES;database=DbNewOopCore1;integrated security=true;");
+        }
+
+        //Veritabanında tablolarımızı oluşturacak kodlar.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
